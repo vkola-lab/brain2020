a13899e6e8ffa58b81b3a6595724f0cd163273a6
shangranq <shangran@bu.edu>
2020-01-16 13:13:37 -0500

    updated csv files

--- lookupcsv/gen_csv.py
+++ lookupcsv/gen_csv.py
@@ -1,6 +1,5 @@
 import csv
 
-
 def read_txt(filename):
     content = []
     with open(filename, 'r') as f:
@@ -9,18 +8,20 @@ def read_txt(filename):
     return content
 
 path = '../../NM/Demor_info/'
-filename = [a[:-4] for a in read_txt(path + 'FHS_Data.txt')]
-status = read_txt(path + 'FHS_Label.txt')
-age = read_txt(path + 'FHS_Age.txt')
-gender = read_txt(path + 'FHS_GENDER.txt')
-mmse = read_txt(path + 'FHS_6_MMSE.txt')
-apoe = read_txt(path + 'FHS_Apoe.txt')
+filename = [a[:-4] for a in read_txt(path + 'NACC_Data.txt')]
+status = read_txt(path + 'NACC_Label.txt')
+age = read_txt(path + 'NACC_Age.txt')
+gender = read_txt(path + 'NACC_GENDER.txt')
+mmse = read_txt(path + 'NACC_MMSE.txt')
+apoe = read_txt(path + 'NACC_Apoe.txt')
+check = read_txt(path + 'NACC_Check_FCN.txt')
 
-with open('FHS.csv', 'w') as csvfile:
+with open('NACC.csv', 'w') as csvfile:
     fieldnames = ['filename', 'status', 'age', 'gender', 'mmse', 'apoe']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     for i in range(len(filename)):
+        if check[i] == 'False': continue
         writer.writerow({'filename': filename[i], 'status': status[i], 'age': age[i], \
                         'gender': gender[i], 'mmse': mmse[i], 'apoe': apoe[i]})
     
